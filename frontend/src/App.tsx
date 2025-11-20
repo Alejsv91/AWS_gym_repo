08a72f9e17f89913a611f1e80694b396221f73e2
Alejsv91 <Alejandro91_cr@hotmail.com>
2025-11-20 10:40:25 -0600

    Adding Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 // App.js
 
 import { useAuth } from "react-oidc-context";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   const auth = useAuth();
