eeb63af476d2ee13046ab90245d0d451d9e43da4
Alejsv91 <Alejandro91_cr@hotmail.com>
2025-11-21 16:17:54 -0600

    Adding required changes to add tokens in API Calls

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from dotenv import load_dotenv
+from fastapi.security import OAuth2PasswordBearer
 import psycopg2
 import os
+import jwt
+import requests
 
 
 load_dotenv()
-
 app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+COGNITO_POOL_ID = os.getenv("COGNITO_POOL_ID")
+COGNITO_REGION = os.getenv("COGNITO_REGION")
+COGNITO_KEYS_URL = f"https://cognito-idp.{COGNITO_REGION}.amazonaws.com/{COGNITO_POOL_ID}/.well-known/jwks.json"
+COGNITO_CLIENT_ID = os.getenv("COGNITO_CLIENT_ID")
+
+jwks = requests.get(COGNITO_KEYS_URL).json()
+
+def verify_token(token: str):
+    try: 
+        decode = jwt.decode(toke, jwks, algorithms=['RS256'], audience=COGNITO_CLIENT_ID)
+        return decode
+    except Exception:
+        raise HTTPException(status_code=401, detail="Invalid token")
 
 def get_connection():
     return psycopg2.connect(
@@ -22,8 +39,9 @@ def read_root():
     return {"message": "Hello from FastAPI"}
 
 @app.get("/roles")
-def get_roles():
+def get_roles(token: str = Depends(oauth2_scheme)):
     try: 
+        payload = verify_token(token)
         conn = get_connection()
         cur = conn.cursor()
         cur.execute("SELECT * FROM roles;")
@@ -36,4 +54,10 @@ def get_roles():
         ]
     except Exception as e:
         return {"error": str(e)}
+    
+@app.get("/protected")
+def protected_route(token: str = Depends(oauth2_scheme)):
+    payload = verify_token(token)
+    return {"message": "You are authorized", "user": payload} 
+
     
---
+++ frontend/src/services/userService.tsx
@@ -0,0 +1,18 @@
+import { useAuth } from "react-oidc-context";
+import { USER_ENDPOINT } from "../constants/endpoints";
+import axios, { InternalAxiosRequestConfig } from "axios";
+
+const auth = useAuth();
+
+export async function getUsers() {
+    const instance = axios.create({
+        baseURL: USER_ENDPOINT.getUsers
+    });
+
+    instance.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+        if(auth.user?.access_token){
+            config.headers.Authorization = `Bearer ${auth.user.access_token}`;
+        }
+        return config;
+    });
+}
