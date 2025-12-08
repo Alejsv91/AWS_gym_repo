6fb052848e8e68eb218c516c36caba5c0e02a49a
Alejsv91 <Alejandro91_cr@hotmail.com>
2025-12-08 17:03:40 -0600

    Adding get endpoint

--- frontend/src/pages/user/UserDetails.tsx
+++ frontend/src/pages/user/UserDetails.tsx
@@ -1,7 +1,11 @@
 import { useParams } from "react-router-dom";
+import { useFetchUserById } from "../../services/userService";
 
 export default function UserDetails() {
   const { id } = useParams<{ id: string }>();
+  const userInfo = useFetchUserById(id!);
+
+  console.log(userInfo);
 
   return (
     <div className="container mt-5">
--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -8,13 +8,30 @@ export function useUsers() {
   const api = useApi();
   const [users, setUsers] = useState<UserResponse[]>([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     const fetchUsers = async () => {
       const { data } = await api.get(USER_ENDPOINT.getUsers);
-      const mappedUsers = data.map(mapUser)
+      const mappedUsers = data.map(mapUser);
       setUsers(mappedUsers);
     };
     fetchUsers();
   }, []);
-  return users; 
+  return users;
+}
+
+export function useFetchUserById(id: string) {
+  const api = useApi();
+  const [user, setUser] = useState<UserResponse | null>(null);
+
+  useEffect(() => {
+    const fetchUserById = async () => {
+      {
+        const { data } = await api.get(USER_ENDPOINT.getUserById(id));
+        const mappedUser = mapUser(data);
+        setUser(mappedUser);
+      }
+    };
+    fetchUserById();
+  }, []);
+  return user;
 }
