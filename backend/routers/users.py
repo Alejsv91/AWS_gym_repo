308e7a829ccf8fc1957ec6e575f0fb351868bfe5
Alejsv91 <Alejandro91_cr@hotmail.com>
2025-12-08 16:39:39 -0600

    SCRUM-24
    Creating the api to get an specific user

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,7 +1,7 @@
 from fastapi.security import HTTPBearer
 from fastapi import APIRouter, Depends
 from core.auth import get_current_user
-from services.users_service import fetch_users
+from services.users_service import fetch_users, fetch_user_by_id
 security = HTTPBearer()
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -10,5 +10,12 @@ router = APIRouter(prefix="/users", tags=["users"])
 def get_users(user=Depends(get_current_user)):
     try: 
         return fetch_users()
+    except Exception as e:
+        return {"error": str(e)}
+    
+@router.get("/{user_id}")
+def get_user_by_id(user_id: int, user=Depends(get_current_user)):
+    try:
+        return fetch_user_by_id(user_id)
     except Exception as e:
         return {"error": str(e)}
--- backend/services/users_service.py
+++ backend/services/users_service.py
@@ -3,6 +3,39 @@ from models.user import User
 from models.role import Role
 from models.identification_type import IdentificationType
 
+def fetch_user_by_id(user_id: int):
+    conn = get_connection()
+    try: 
+        cur = conn.cursor()
+        query = """
+        SELECT 
+        u.id,
+        u.first_name,
+        u.last_name,
+        u.id_number,
+        u.phone_number,
+        u.email,
+        u.address,
+        u.nationality,
+        r.id AS role_id,
+        r.name AS role_name,
+        r.description AS role_description,
+        it.id AS identification_type_id,
+        it.name AS identification_type_name,
+        it.description AS identification_type_description
+        FROM users u
+        JOIN roles r ON u.role_id = r.id
+        JOIN identification_type it ON u.identification_type_id = it.id
+        WHERE u.id = %s;
+        """
+        cur.execute(query, (user_id,))
+        row = cur.fetchone()
+        user = create_user_object(row)
+        return user
+    finally:
+        cur.close()
+        conn.close()
+
 def fetch_users():
     conn = get_connection()
     try:
@@ -21,24 +54,28 @@ def fetch_users():
         rows = cur.fetchall()
         users = []
         for row in rows:
-            role = Role(id=row[8], name=row[9], description=row[10])
-            identification_type = IdentificationType(
-                id=row[11], name=row[12], description=row[13]
-            )
-            user = User(
-                id=row[0],
-                first_name=row[1],
-                last_name=row[2],  
-                id_number=row[3],
-                phone_number=row[4],
-                email=row[5],
-                address=row[6],
-                nationality=row[7], 
-                role=role,
-                identification_type=identification_type
-            )
+            user = create_user_object(row)
             users.append(user)
         return users
     finally:
         cur.close()
         conn.close()
+
+def create_user_object(row) -> User:
+    role = Role(id=row[8], name=row[9], description=row[10])
+    identification_type = IdentificationType(
+        id=row[11], name=row[12], description=row[13]
+    )
+    user = User(
+        id=row[0],
+        first_name=row[1],
+        last_name=row[2],  
+        id_number=row[3],
+        phone_number=row[4],
+        email=row[5],
+        address=row[6],
+        nationality=row[7], 
+        role=role,
+        identification_type=identification_type
+    )
+    return user
