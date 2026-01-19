5bc885f8698b98d69b2b500df13696bf976a2623
Alejsv91 <Alejandro91_cr@hotmail.com>
2026-01-19 11:42:27 -0600

    Adding remove the user form cognito and database

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -20,7 +20,7 @@ export function useDeleteUser() {
   const deleteUser = async (id: string) => {
     if (window.confirm("Are you sure you want to delete this user?")) {
       const result = await api.delete(USER_ENDPOINT.deleteUser(id));
-      if (result.status !== 200) {
+      if (result.status !== 204) {
         console.error("Failed to delete user:", result.status, result.data);
         alert("Failed to delete user.");
         return false;
